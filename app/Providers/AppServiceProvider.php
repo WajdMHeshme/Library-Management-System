328184e4abae2dbd0860a8857ad6a1eb00ac8c75
unknown <wajdtitos@gmail.com>
2026-04-01 22:55:38 +0300

    refactor: extract repository bindings into RepositoryServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,21 +1,6 @@
 <?php
 
 namespace App\Providers;
-
-use App\Repositories\Contracts\AuthRepositoryInterface;
-use App\Repositories\Contracts\BookRepositoryInterface;
-use App\Repositories\Contracts\BorrowingRepositoryInterface;
-use App\Repositories\Contracts\CategoryRepositoryInterface;
-use App\Repositories\Contracts\FAQRepositoryInterface;
-use App\Repositories\Contracts\ReviewRepositoryInterface;
-use App\Repositories\Contracts\UserRepositoryInterface;
-use App\Repositories\Eloquent\AuthRepository;
-use App\Repositories\Eloquent\BookRepository;
-use App\Repositories\Eloquent\BorrowingRepository;
-use App\Repositories\Eloquent\CategoryRepository;
-use App\Repositories\Eloquent\FAQRepository;
-use App\Repositories\Eloquent\ReviewRepository;
-use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -26,40 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(
-            AuthRepositoryInterface::class,
-            AuthRepository::class
-        );
 
-        $this->app->bind(
-            BookRepositoryInterface::class,
-            BookRepository::class
-        );
-
-        $this->app->bind(
-            CategoryRepositoryInterface::class,
-            CategoryRepository::class
-        );
-
-        $this->app->bind(
-            FAQRepositoryInterface::class,
-            FAQRepository::class
-        );
-
-        $this->app->bind(
-            UserRepositoryInterface::class,
-            UserRepository::class
-        );
-
-        $this->app->bind(
-            BorrowingRepositoryInterface::class,
-            BorrowingRepository::class
-        );
-
-        $this->app->bind(
-            ReviewRepositoryInterface::class,
-            ReviewRepository::class
-        );
     }
 
     /**
